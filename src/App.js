04823849d9a4df369048ced90e6d0e02fff4a7c5
jimmymetts <jimmymetts@bellsouth.net>
2020-05-09 12:45:09 -0500

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './App.css';
+import Axios from 'axios';
 
 
 class App extends React.Component {
 
+  //state
+  state = {
+    coords:{
+      latitude: 45,
+      longitude: 60
+    }
+  }
+  
   /* lifcycle method */
 
   componentDidMount() {
     console.log("Mounted");
+
     //get device location
-    if (navigator.geolocation){
-      //console.log("Supported")
+
+    if (navigator.geolocation) {
+                                      //console.log("Supported")
       navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position.coords.latitude);
+        let newCoords = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+        this.setState({ coords: newCoords });
+                                      //console.log(position.coords.longitude );
       })
     } else{ 
       console.log("Not Supported")
