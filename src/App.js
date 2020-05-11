87a7c1d9f442cc727ccb54936e0d59c15a64a1a2
jimmymetts <jimmymetts@bellsouth.net>
2020-05-11 08:45:43 -0500

    adding weather details to const weatherData

--- src/App.js
+++ src/App.js
@@ -32,6 +32,18 @@ class App extends React.Component {
         //api call
         Axios.get(`http://api.weatherstack.com/current?access_key=3e3ff8f35d948794fe3b66925fce0e7c&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res => {
           console.log(res)
+          
+          const weatherData = {
+            location:res.data.location.name,
+            temperature:res.data.current.temperature,
+            description:res.data.current.weather_descriptions[0],
+            region:res.data.location.region,
+            country:res.data.location.country,
+            wind_speed:res.data.current.wind_speed,
+            
+
+
+          }
         })
       })
     } else{ 
